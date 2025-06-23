bdeac9146511ca6c62e6168cabf0967a1699a0f5
Mayo Layo <l.t05@outlook.com>
2025-06-24 00:35:48 +0200

    Rough sktech of layout

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import battle_functions
+import sys
 
 def main():
-    pass
+    print("Type help for all commands, type exit to close")
+    while True:
+        user_input = input("")
+        user_input = remove_special_chars(user_input).lower().split()
+        first_word = user_input[0]
+        if first_word == "help":
+            print_available_functions()
+        if first_word == "weakness":
+            pass
+        if first_word == "exit":
+            sys.exit()
+    
 
+def print_available_functions() -> None:
+    pass
 
 def remove_special_chars(input_string: str) -> str:
     """Removes special characters from a given string
