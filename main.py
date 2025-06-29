911b2e3ec594eb881be46c1bdc25734ac9a67fc3
Mayo Layo <l.t05@outlook.com>
2025-06-29 15:54:01 +0200

    Made main be able to get weaknesses of specific pokemon

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import battle_functions
 import sys
 from os import system
 
-clear_words = ["clear", "clr", "c"]
+clear_words = ("clear", "clr", "c")
+weakness_words = ("w", "weak", "weakness")
 
 def main():
     print("Type help for all commands, type exit to close")
@@ -12,15 +13,25 @@ def main():
         first_word = user_input[0]
         if first_word == "help":
             if len(user_input) == 1:
+                print_new_line()
                 print_available_functions()
             else:
                 print("Not available right now")
-        elif first_word == "weakness":
-            pass
+
+        elif first_word in weakness_words:
+            print_new_line()
+            battle_functions.print_coverage_for_pokemon(user_input[1])
+
         elif first_word in clear_words:
-            system("clr")
+            system('cls')
+            print("Type help for all commands, type exit to close")
+
         elif first_word == "exit":
             sys.exit()
+            
+        else:
+            print_new_line()
+            print("command not found")
 
 
 def print_available_functions() -> None:
@@ -40,6 +51,10 @@ def remove_special_chars(input_string: str) -> str:
             clean_string += char
     return clean_string
 
+
+def print_new_line() -> None:
+    '''Prints a new line in terminal'''
+    print("")
 
 
 if __name__ == '__main__':
